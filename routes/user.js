f1f34bf653b3e7630263003569870a0b2f3df1e1
Samuli Asmala <samuli.asmala@gmail.com>
2019-11-17 15:25:07 +0200

    Implement create user API endpoint

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,10 @@
 var express = require('express');
+var createError = require('http-errors');
 var router = express.Router();
+
+const log = require('../utils/logger');
+
+const UserService = require('../services/UserService');
 
 /**
  * @api {get} /user Get details of the current (logged in) user
@@ -40,6 +45,48 @@ router.get('/', (req, res, next) => {
     });
   } else {
     return res.status(401).json({ error: 'UserNotLoggedIn' });
+  }
+});
+
+/**
+ * @api {post} /user/create Create new user
+ * @apiName CreateUser
+ * @apiGroup User
+ *
+ * @apiParam {String}   username User's username (i.e. user's primary email)
+ * @apiParam {String}   password User's password
+ * @apiParam {String}   [default_reminder_email] User's default reminder email if not equal to username
+ * @apiParam {Number}   [year_born] User's birth year
+ *
+ * @apiSuccessExample {json} Success-Response:
+ *     HTTP/1.1 200 OK
+ *     {
+ *       "username": "samuli",
+ *       "username": "passwd",
+ *       "year_born": "1900"
+ *     }
+ */
+router.post('/create', async (req, res, next) => {
+  try {
+    let { username, password, default_reminder_email, year_born } = req.body;
+    log.debug(`Create user`, { username, default_reminder_email, year_born });
+    let user = await UserService.createUser(
+      username,
+      password,
+      default_reminder_email,
+      year_born
+    );
+    if (user != null && user.error == null) {
+      log.info(`New user created`, { id: user.id, username: user.username });
+      return res.status(200).json({
+        id: user.id
+      });
+    } else {
+      log.info(`Unable to create user: ${user.message}`);
+      next(createError(400, user.message));
+    }
+  } catch (err) {
+    next(err);
   }
 });
 
--- services/UserService.js
+++ services/UserService.js
@@ -1,12 +1,50 @@
+const bcrypt = require('bcrypt');
+
 const log = require('../utils/logger');
-const { userModel } = require('../models/user');
+const { User } = require('../models');
+
+const saltRounds = 10;
 
 async function findUserById(id) {
-  log.debug(id, 'Finding user by id');
-  const user = await userModel.findById(id);
+  log.debug('Finding user by id', { id });
+  const user = await User.findByPk(id);
+  return user;
+}
+
+async function createUser(username, password, reminderEmail, yearBorn) {
+  log.debug('Creating new user', { username });
+
+  // Check if user exists already
+  let existingUser = await findByUsername(username);
+  if (existingUser != null) {
+    return {
+      error: true,
+      message: 'Username exists already'
+    };
+  }
+
+  let hash = await bcrypt.hash(password, saltRounds);
+  let user = await User.create({
+    username: username,
+    password_hash: hash,
+    default_reminder_email: reminderEmail,
+    year_born: yearBorn
+  });
+  return user;
+}
+
+async function findByUsername(username) {
+  log.debug('Finding user by username', { username });
+  const user = await User.findOne({
+    where: {
+      username: username
+    }
+  });
   return user;
 }
 
 module.exports = {
-  findUserById
+  findUserById,
+  createUser,
+  findByUsername
 };
